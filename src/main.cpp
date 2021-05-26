2d568bd5f9235d58b0a0e9f2c18ab8b9db0be48b
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-05-26 03:23:16 -0700

    used PlatformIO to add the FreeRTOS lib. manually created CMakeListsUser.txt so the IDE can see the path to this lib for real-time inspection. prior to adding the app/client code, just doing an led and variable timing blink test on the uno. confirmed the on-board led is pin 13.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
+/*
+ * Tutorial: https://circuitdigest.com/microcontroller-projects/arduino-freertos-tutorial1-creating-freertos-task-to-blink-led-in-arduino-uno
+ */
+
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+
 void setup() {
-// write your initialization code here
+    pinMode(13, OUTPUT);
 }
 
+int del = 20;
+int dir = 1; // 1 up, 0 down
+
 void loop() {
-// write your code here
+    digitalWrite(13, HIGH);
+    delay(del);
+    digitalWrite(13, LOW);
+    delay(del);
+    if (dir == 0) { del = del - 20; }
+    else { del = del + 20; }
+    if (del < 20) { dir = 1; del = 20; }
+    if (del > 240) {dir = 0; del = 240; }
 }
+
+//void setup() {
+//// write your initialization code here
+//}
+//
+//void loop() {
+//// write your code here
+//}
