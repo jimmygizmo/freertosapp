aaeb6c338652bfb2938cd0c2b7622ae89c30f46f
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-05-26 22:59:27 -0700

    refactored out for full configurability of dynamic blinking with documenting code comments. really cleaned up all code and fully decorated and documented/commented it according to my own and community best practices (some general refactoring too). added headings for the main code/file sections, which I often like to do. looks very nice and makes for very readable code. helps beginners too and many of my projects are good for beginners.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,108 @@
-/*
- * Tutorial: https://circuitdigest.com/microcontroller-projects/arduino-freertos-tutorial1-creating-freertos-task-to-blink-led-in-arduino-uno
+/* APP NAME: freertosapp
+ * AUTHOR: Jimmy Gizmo
+ * REPO: git@github.com/jimmygizmo/freertosapp
+ * WEBSITE: https://ninthdevice.com
+ * DESCRIPTION AND OTHER INFO: (See end of file)
+ * HARDWARE: OSEPP Arduino UNO R4 (Rev 4.0) (or any UNO or Mega), Velleman VMA203 LCD Keypad (or any similar/common)
+ * PLATFORM, LANGUAGES (Versions approx.): Arduino, FreeRTOS v10.4.3-8, C++ v TODO: Finish
+ * TOOLS (Versions approx.): PlatformIO v5.1.1, TODO: Finish
  */
+/****************************************************** INCLUDES ******************************************************/
 
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
 
-void setup() {
-    pinMode(13, OUTPUT);
-}
 
-int del = 20;
-int dir = 1; // 1 up, 0 down
+/******************************************* CONFIGURATION & INITIALIZATION *******************************************/
+
+int delay_delta = 10;  // The delta value in milliseconds by which blink_delay is either incremented or decremented.
+
+int blink_delay = 10;  // Starting value for blink delay in milliseconds. blink_delay will change dynamically.
+
+int delay_min = 10;  // Minimum blink_delay value in milliseconds before the delta_dir will change to ascending (1).
+
+int delay_max = 180;  // Maximum blink_delay value in milliseconds before the delta_dir will change to descending (0).
+
+int delta_dir = 1;  // Initial direction for blink_delay increment/decrement: 1 up (add), 0 down (subtract)
+// Up/increment/add means the delays get longer. Down/decrement/subtract mean the delays get shorter.
+// The same delay value is used for LED <ON> time as for the <OFF> time, which are two different delay events.
+
+int led_pin = 13;
+
+
+/************************************************ FUNCTION DEFINITIONS ************************************************/
+
+// Application-specific functions go here, where they must be defined before possible usage in setup() or loop().
+
+
+/*************************************************** ARDUINO SETUP ****************************************************/
+
+void setup() {
+    pinMode(led_pin, OUTPUT);
+
+} /* setup() */
+
+
+/************************************************* ARDUINO MAIN LOOP **************************************************/
 
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(del);
-    digitalWrite(13, LOW);
-    delay(del);
-    if (dir == 0) { del = del - 20; }
-    else { del = del + 20; }
-    if (del < 20) { dir = 1; del = 20; }
-    if (del > 240) {dir = 0; del = 240; }
-}
+    // Currently this is just a simple blink test that does not actually use FreeRTOS yet.
+    digitalWrite(led_pin, HIGH);
+    delay(blink_delay);
+    digitalWrite(led_pin, LOW);
+    delay(blink_delay);
 
-//void setup() {
-//// write your initialization code here
-//}
-//
-//void loop() {
-//// write your code here
-//}
+    if (delta_dir == 0) {
+        blink_delay = blink_delay - delay_delta;
+    }
+    else {
+        blink_delay = blink_delay + delay_delta;
+    }
+
+    if (blink_delay < delay_min) {
+        delta_dir = 1;
+        blink_delay = delay_min;
+    }
+    if (blink_delay > delay_max) {
+        delta_dir = 0;
+        blink_delay = delay_max;
+    }
+
+} /* loop() */
+
+
+/******************************************************* NOTES ********************************************************/
+
+/*
+ * DESCRIPTION:
+ * This is my basic implementation of Arduino FreeRTOS (Real Time Operating System) on an UNO, as a starting point
+ * for later specific, utilitarian purposes, initially this will involve basic blink, serial monitor output and
+ * possibly the use of an LCD Keypad as I would like to develop a nice real-time, non-blocking menu system for
+ * Arduino + LCDKeypad. I think this extremely affordable hardware combo has massive value potential for so many
+ * kinds of projects. It all hinges on being non-blocking, in my opinion, because even though the LCDKeypad has
+ * great potential for human I/O, this is NOT true if it remains single-threaded/blocking, for pretty-much any
+ * kind of project. Yes, you could have a single Arduino running only the LCD Keypad menu I/O system and then
+ * communicate with 1 or more other Arduinos or whatever, over serial or circuit logic or whatever, but there
+ * is much value in being able to use an RTOS to free up the Arduino to do its most important work, while
+ * simultaneously running a menu system, that does not necessarily need much CPU work; it simply needs real-time
+ * responsiveness, while allowing other work to proceed with little or no interference between those separate
+ * but simultaneous concerns.
+ *
+ */
+
+
+/* NOTES:
+ * Tutorial behind some of this work:
+ * https://circuitdigest.com/microcontroller-projects/arduino-freertos-tutorial1-creating-freertos-task-to-blink-led-in-arduino-uno
+ *
+ * FreeRTOS Arduino Reference/Documentation:
+ * https://www.arduino.cc/reference/en/libraries/freertos/
+ *
+ * Interesting tutorial that should be looked at, maybe referenced in this project (we'll see):
+ * https://create.arduino.cc/projecthub/feilipu/using-freertos-multi-tasking-in-arduino-ebc3cc
+ * It is a simple blink using FreeRTOS. They say it is generic for any Arduino and dont specify what board.
+ *
+ */
+
+
+/**/
